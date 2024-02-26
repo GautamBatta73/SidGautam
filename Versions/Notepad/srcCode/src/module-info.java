be614c6344f75b489835062ab0a8559491c6d1dc
Gautam Batta <119340575+GautamBatta73@users.noreply.github.com>
2024-02-25 20:52:53 -0500

    Updated SidGautamNotes

--- Versions/Notepad/srcCode/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module SidGautamNotes {
-	requires javafx.controls;
-	requires javafx.web;
-	requires javafx.graphics;
-	requires javafx.base;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
